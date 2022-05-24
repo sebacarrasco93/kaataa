edf01f3482bfd32291116fad80e9518f8db56cbc
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2022-05-24 08:44:24 -0400

    Refactor

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -3,6 +3,8 @@
 namespace SebaCarrasco93\Kaataa\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class BaseCommand extends Command
 {
@@ -19,5 +21,38 @@ class BaseCommand extends Command
     public function invalid()
     {
         return Command::INVALID;
+    }
+
+    public function showSuccess(OutputInterface $output, $message = null)
+    {
+        if ($message) {
+            $output->writeln("✅ {$message}");
+        } else {
+            $output->writeln('✅ Done');
+        }
+
+        return $this->failure();
+    }
+
+    public function showError(OutputInterface $output, $message = null)
+    {
+        if ($message) {
+            $output->writeln("❌ {$message}");
+        } else {
+            $output->writeln("❌ An error was found");
+        }
+
+        return $this->failure();
+    }
+
+    public function showInvalid(OutputInterface $output, $message = null)
+    {
+        if ($message) {
+            $output->writeln("❌ {$message}");
+        } else {
+            $output->writeln('❌ Please provide the name');
+        }
+
+        return $this->invalid();
     }
 }
--- src/Commands/CreateCommand.php
+++ src/Commands/CreateCommand.php
@@ -14,11 +14,11 @@ class CreateCommand extends BaseCommand
 
     protected function configure(): void
     {
-        $this->setDescription('Create a project')
-            ->setHelp('Create a Class and a Test file')
+        $this->setDescription('Create class and test files')
+            ->setHelp('Create a class and a test file')
             ->addArgument('name',
                 $this->name ? InputArgument::REQUIRED : InputArgument::OPTIONAL,
-                'Specify their name'
+                'Specify the name for class and test file'
             )
         ;
     }
@@ -26,34 +26,16 @@ class CreateCommand extends BaseCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         if ($name = $input->getArgument('name')) {
-            return $this->intentarCrearProyecto($output, $name);
+            return $this->createProject($output, $name);
         }
 
-        return $this->mostrarInvalidoProyectoSinname($output);
+        return $this->showInvalid($output);
     }
 
-    public function intentarCrearProyecto(OutputInterface $output, $name)
+    public function createProject(OutputInterface $output, $name)
     {
-        $output->writeln("✅ Se creó el proyecto {$name}!");
+        $this->showSuccess($output, "Your file {$name} was created successfully");
 
         return $this->success();
-    }
-
-    public function mostrarError(OutputInterface $output, $mensaje = null)
-    {
-        $output->writeln('❌ Se produjo un error...');
-
-        if ($mensaje) {
-            $output->writeln($mensaje);
-        }
-
-        return $this->failure();
-    }
-
-    public function mostrarInvalidoProyectoSinname(OutputInterface $output)
-    {
-        $output->writeln('❌ Please provide the name');
-
-        return $this->invalid();
     }
 }
