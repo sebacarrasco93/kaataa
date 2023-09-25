1055140051fa9645b168d3cee680e421ba0bc5cb
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2023-09-24 23:38:30 -0300

    Add helpers.php

---
+++ extra/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ * This file is autoloaded
+ *
+ * Here you can by start to writing your own helpers
+ * Helpers can be used anywhere in your Kaataa project.
+ *
+ */
+
+function your_custom_function()
+{
+    //
+}
